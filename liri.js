955b8f6209f4d3a3cd091ad7d9f0ecf77908a454
Adam Goings <adamgoings@Adams-MacBook-Pro.local>
2018-04-30 14:43:02 -0400

    all four nodes working appropriately

--- liri.js
+++ liri.js
@@ -30,20 +30,70 @@ if (process.argv[2] === "my-tweets") {
 //=============================
 //Spotify Portion of LIRI
 //=============================
+
+//pulling in spotify npm and keys
 var Spotify = require('node-spotify-api');
 
 if (process.argv[2] === "spotify-this-song") {
   var spotify = new Spotify(keys.spotify);
 
-
+  //placing the GET call to the Spotify API
   spotify.search({ type: 'track', query: process.argv[3], limit: 1 }, function (err, data) {
     if (err) {
-        console.log('Error occurred: ' + err);
+      console.log('Error occurred: ' + err);
     } else {
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Track: " + data.tracks.items[0].name);
+      console.log("Preview: " + data.tracks.items[0].preview_url);
+      console.log("Album: " + data.tracks.items[0].album.name);
+    }
+  });
+}
+
+//=============================
+//OMDB Portion of LIRI
+//=============================
+var request = require('request');
+
+if (process.argv[2] === "movie-this") {
+  request('http://www.omdbapi.com/?apikey=trilogy&t=' + process.argv[3], function (error, response, body) {
+    console.log(error);
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Year: " + JSON.parse(body).Year);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+    console.log("Country: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+  });
+}
+
+
+//=============================
+//"Do-What-It-Says" Portion of LIRI
+//=============================
+var fs = require("fs");
+
+if (process.argv[2] === "do-what-it-says") {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      console.log(error);
+    } else {
+      //console.log(data);
+      var dataArr = data.split(",");
+      // We will then re-display the content as an array for later use.
+      console.log(dataArr);
+
+      var spotify = new Spotify(keys.spotify);
+
+      //placing the GET call to the Spotify API
+      spotify.search({ type: 'track', query: dataArr[1], limit: 1 }, function (err, data) {
         console.log("Artist: " + data.tracks.items[0].artists[0].name);
         console.log("Track: " + data.tracks.items[0].name);
         console.log("Preview: " + data.tracks.items[0].preview_url);
         console.log("Album: " + data.tracks.items[0].album.name);
+      });
     }
   });
 }
